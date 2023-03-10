a3f37beb72db08992d291583d19d92d9486f3241
Mailandr <splintersfinest@gmail.com>
2023-03-10 17:08:35 +0100

    test

--- convertStringToCamelCase.js
+++ convertStringToCamelCase.js
@@ -3,7 +3,16 @@ const text2 = "The_Stealth_Warrior";
 
 function toCamelCase(str) {
   let newText1 = str.replace(/_/g, "-").split('-');
-  console.log(newText1);
+  const wordsUpper = [];
+
+  for (const x of newText1) {
+    wordsUpper.push(x[0].toUpperCase() + x.slice(1));
+  }
+  if (str[0] === str[0].toLowerCase) {
+    console.log(wordsUpper[0].toLowerCase + wordsUpper.slice(1));
+  } else {
+    console.log(wordsUpper.join(''));
+  }
 }
 
 toCamelCase(text1);
