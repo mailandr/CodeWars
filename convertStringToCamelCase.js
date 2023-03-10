0c769232547948c0d55fc6a245b93929ce02e0b3
Mailandr <splintersfinest@gmail.com>
2023-03-10 16:18:20 +0100

    split function angewendet

--- convertStringToCamelCase.js
+++ convertStringToCamelCase.js
@@ -2,9 +2,8 @@ const text1 = "the-stealth-warrior";
 const text2 = "The_Stealth_Warrior";
 
 function toCamelCase(str) {
-  let newText1 = str.replace(/-/g, " ");
-  let newText2 = newText1.replace(/_/g, " ");
-  console.log(newText2);
+  let newText1 = str.replace(/_/g, "-").split('-');
+  console.log(newText1);
 }
 
 toCamelCase(text1);
