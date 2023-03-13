3664b955dc847b6e9d4af9e87a0fe2a2a83a2693
Milan <37807335+mailandr@users.noreply.github.com>
2023-03-13 10:20:09 +0100

    solved

--- convertStringToCamelCase.js
+++ convertStringToCamelCase.js
@@ -1,17 +1,23 @@
+"use strict";
+
 const text1 = "the-stealth-warrior";
 const text2 = "The_Stealth_Warrior";
 
 function toCamelCase(str) {
-  let newText1 = str.replace(/_/g, "-").split('-');
+  let newText1 = str.replace(/_/g, "-").split("-");
+  if (str < 1) return str;
   const wordsUpper = [];
 
   for (const x of newText1) {
     wordsUpper.push(x.replace(x[0], x[0].toUpperCase()));
   }
-  let finishedText = wordsUpper.join('');
+  let finishedText = wordsUpper.join("");
 
   if (str[0] !== finishedText[0]) {
-    const finText = finishedText.replace(finishedText[0], finishedText[0].toLowerCase());
+    const finText = finishedText.replace(
+      finishedText[0],
+      finishedText[0].toLowerCase()
+    );
     return finText;
   }
   return finishedText;
